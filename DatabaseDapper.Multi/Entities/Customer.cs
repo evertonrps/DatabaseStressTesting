a6002b53b662954798867c6297d50d7374cc5bf1
Everton Rocha <evertonrps@gmail.com>
2023-03-07 06:50:40 -0300

    tratamento Name nulo

--- DatabaseDapper.Multi/Entities/Customer.cs
+++ DatabaseDapper.Multi/Entities/Customer.cs
@@ -3,7 +3,7 @@
 public class Customer
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
     public int Age { get; set; }
     public DateTime CreateAt { get; set; }
 }
